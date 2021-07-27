21f004299fbc9d5f473d0996f542900bc9351eb0
Ramon Cuñat <cunyat.r@gmail.com>
2021-07-27 20:11:37 +0200

    improve and document events

--- internal/common/domain/event/event.go
+++ internal/common/domain/event/event.go
@@ -7,28 +7,37 @@ import (
 	"github.com/google/uuid"
 )
 
+// Type represents a unique identifier for each type of event
 type Type string
 
+// Event represents the behaviour of a Domain Event
 type Event interface {
-	UUID() string
+	EventUUID() string
 	AggregateUUID() string
 	OccurredOn() time.Time
 	EventType() Type
 }
 
-type EventListener func(context.Context, Event) error
+// Listener defines the signature of an event listener
+type Listener func(context.Context, Event) error
 
+// Bus defines the behaviour of the Event Bus
 type Bus interface {
-	Subscribe(Type, EventListener)
-	Publish(context.Context, Event) error
+	// Subscribe register a listener for the given event type
+	Subscribe(Type, Listener)
+
+	// Publish sends events to their listeners
+	Publish(context.Context, ...Event) error
 }
 
+// BaseEvent gives the basic & common features of a domain event
 type BaseEvent struct {
 	eventUUID     string
 	aggregateUUID string
 	occurredOn    time.Time
 }
 
+// Generates a new BaseEvent for the given aggregate (as uuid)
 func NewBaseEvent(aggregateUUID string) BaseEvent {
 	return BaseEvent{
 		eventUUID:     uuid.NewString(),
@@ -37,14 +46,17 @@ func NewBaseEvent(aggregateUUID string) BaseEvent {
 	}
 }
 
-func (b BaseEvent) UUID() string {
+// EventUUID return the current event identifier
+func (b BaseEvent) EventUUID() string {
 	return b.eventUUID
 }
 
+// OccurredOn represents the moment that the event has been fired
 func (b BaseEvent) OccurredOn() time.Time {
 	return b.occurredOn
 }
 
+// AggregateUUID represents the aggregate that fired a given event
 func (b BaseEvent) AggregateUUID() string {
 	return b.aggregateUUID
 }
